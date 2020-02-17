5ade6e5662fdbc406fdbb5235f347298592edf48
Artemis <hollyshort21@gmail.com>
2020-02-17 21:27:18 +0000

    added edit command to arenamodbot

--- arenabot/mods.py
+++ arenabot/mods.py
@@ -51,6 +51,11 @@ class Modifiers:
         cls.save()
 
     @classmethod
+    def edit(cls, num, field, new):
+        cls.mods[num-1][field] = new
+        cls.save()
+
+    @classmethod
     def save(cls):
         data = {'mods': cls.mods}
         with open('mods.json', 'w') as f:
@@ -93,11 +98,36 @@ async def all_(ctx):
 @bot.command(brief='Add a new modifier.')
 @admins()
 async def new(ctx, name, turns: int, *, desc):
-    '''Add a new modifier. May only be used by arena admins. A value of 0 for turns means infinity. Examples:
+    '''Add a new modifier. May only be used by arena admins. A value of 0 for \
+    turns means infinity. Examples:
     `mod new "Modifier Name" 0 The modifier description.`
     `mod new OneWordName 3 This modifier lasts for 3 turns.`
     '''
     Modifiers.add(name, desc, turns)
+    await ctx.send('Done!')
+
+
+@bot.command(brief='Edit a modifier.', name='edit')
+@admins()
+async def edit(ctx, num: int, field, *, new):
+    '''Edit a modifier by number. May only be used by arena admins. Example:
+    `mod edit 3 name New Modifier Name`
+    `mod edit 4 desc The new modifier description.`
+    `mod edit 8 turns 0`
+    See numbers with the `mod all` command. `field` must be `name`, `desc` or \
+    `turns`.
+    '''
+    if num < 1:
+        return await ctx.send('Modifier numbers are all positive...')
+    if field not in ('name', 'desc', 'turns'):
+        return await ctx.send(
+            '`field` must be `name`, `desc` or `turns`. Do `mod help edit` '
+            'for more info.'
+        )
+    try:
+        Modifiers.edit(num, field, new)
+    except IndexError:
+        return await ctx.send('Modifier not found.')
     await ctx.send('Done!')
 
 
