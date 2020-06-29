66df13d048dcef72e76962110d49450bf7a70a54
Artemis21 <hollyshort21@gmail.com>
2020-06-29 22:28:18 +0100

    sheet name change

--- polytourney20/tools/sheetsapi.py
+++ polytourney20/tools/sheetsapi.py
@@ -16,7 +16,7 @@ SCOPE = [
 
 CREDS = ServiceAccountCredentials.from_json_keyfile_name('creds.json', SCOPE)
 client = gspread.authorize(CREDS)
-spread_sheet = client.open('ffatourney1')
+spread_sheet = client.open('Polytopia Supreme Summer Skirmish Sheet')
 
 Player = namedtuple(
     'Player',
