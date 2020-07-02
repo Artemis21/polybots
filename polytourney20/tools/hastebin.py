94af033fa3b51ca53828818eae1afc6fbbf5d19b
Artemis21 <hollyshort21@gmail.com>
2020-07-02 16:17:44 +0100

    allow uploading unicode

--- polytourney20/tools/hastebin.py
+++ polytourney20/tools/hastebin.py
@@ -7,6 +7,6 @@ URL = 'https://hastebin.com'
 
 def upload(data: str) -> str:
     """Upload some data to hastebin."""
-    resp = requests.post(f'{URL}/documents', data=data)
+    resp = requests.post(f'{URL}/documents', data=data.encode('utf-8'))
     key = resp.json()['key']
     return f'{URL}/raw/{key}'
