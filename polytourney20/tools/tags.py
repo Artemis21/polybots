3cf0ceed3fb0208fce5ac6c6feb919c403e0d49d
Artemis21 <hollyshort21@gmail.com>
2020-07-03 21:09:17 +0100

    added tags

---
+++ polytourney20/tools/tags.py
@@ -0,0 +1,28 @@
+"""Tags storage/retrieval."""
+import json
+import os
+
+
+FILE = 'data/tags.json'
+
+if not os.path.exists(FILE):
+    open(FILE, 'w').close()
+
+
+def _get_data():
+    """Get all the data."""
+    with open(FILE) as f:
+        return json.load(f)
+
+
+def create_tag(name: str, content: str):
+    """Create a tag."""
+    data = _get_data()
+    data[name.lower()] = content
+    with open(FILE, 'w') as f:
+        json.dump(data, f)
+
+
+def get_tag(name: str):
+    """Get a tag."""
+    return _get_data().get(name.lower(), 'Tag not found.')
