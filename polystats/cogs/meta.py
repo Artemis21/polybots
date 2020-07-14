b7cd73b02670a79b54383fc0b36ff59cdc070aec
Artemis21 <hollyshort21@gmail.com>
2020-07-14 22:11:52 +0100

    more general polishing

--- polystats/cogs/meta.py
+++ polystats/cogs/meta.py
@@ -2,6 +2,8 @@
 from discord.ext import commands
 import discord
 
+from tools.errors import on_command_error
+
 
 SOURCE = 'https://github.com/Artemis21/polybots/tree/master/polystats'
 ABOUT = (
@@ -19,6 +21,23 @@ class Meta(commands.Cog):
         """Store a reference to the bot."""
         self.bot = bot
         bot.help_command.cog = self
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        """Send prefix if bot is mentioned."""
+        if message.guild:
+            me = message.guild.me
+        else:
+            me = self.bot.user
+        if me in message.mentions:
+            await message.channel.send(
+                f'My prefix is `{self.bot.command_prefix}`.'
+            )
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: Exception):
+        """Handle an error."""
+        await on_command_error(ctx, error)
 
     @commands.command(brief='About the bot.')
     async def about(self, ctx: commands.Context):
