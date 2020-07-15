c2289150e4445b96305fd066f7c5d5ddec8869e1
Artemis21 <hollyshort21@gmail.com>
2020-07-15 19:50:46 +0100

    added tasks

--- polystats/tools/objects.py
+++ polystats/tools/objects.py
@@ -7,6 +7,15 @@ import discord
 from discord.ext import commands
 
 
+def neaten(raw: str) -> str:
+    """Neaten a string."""
+    parts = str(raw).split('_')
+    new_parts = []
+    for part in parts:
+        new_parts.append(part[0].upper() + part[1:])
+    return ' '.join(new_parts)
+
+
 @lru_cache(8)
 def load_data(object_type: str) -> typing.Dict:
     """Load data from the file."""
@@ -19,7 +28,13 @@ def list_all(object_type: str) -> str:
     """List short codes and names for a list."""
     lines = []
     for unit in load_data(object_type).values():
-        lines.append('`{code}`: {name}'.format(**unit))
+        line = '`{code}`: {name}'.format(**unit)
+        if 'alt_search' in unit:
+            line += ' (`{alt_code}` | {alt_name})'.format(
+                alt_code=unit['alt_code'],
+                alt_name=neaten(unit['alt_search'])
+            )
+        lines.append(line)
     return '\n'.join(lines)
 
 
@@ -40,7 +55,10 @@ def embed(
     e.set_footer(text=' | '.join(
         f'{field.title()}: {obj[field]}' for field in footer_fields
     ))
-    ignore_fields = [*footer_fields, 'name', 'description', 'image']
+    ignore_fields = [
+        *footer_fields, 'name', 'description', 'image', 'alt_code',
+        'alt_search'
+    ]
     for field in obj:
         if field not in ignore_fields:
             value = obj[field]
@@ -48,9 +66,7 @@ def embed(
                 parts = value or ['None']
             else:
                 parts = [value]
-            value = ', '.join(
-                str(part).title().replace('_', ' ') for part in parts
-            )
+            value = ', '.join(neaten(part) for part in parts)
             e.add_field(name=field.title(), value=value)
     return e
 
@@ -59,13 +75,16 @@ def embed(
 def lookup(object_type: str, search: str) -> typing.Optional[str]:
     """Lookup an object by user search."""
     data = load_data(object_type)
-    search = search.lower().replace(' ', '_').replace('-', '_')
+    search = search.lower().replace(' ', '').replace('-', '').replace("'", '')
     for full_name in data:
-        if search == data[full_name]['code']:
+        codes = (data[full_name]['code'], data[full_name].get('alt_code'))
+        if search in codes:
             return full_name
     possible = []
     for full_name in data:
-        if search in full_name:
+        if search in full_name.replace('_', ''):
+            possible.append(full_name)
+        elif search in data[full_name]['alt_search'].replace('_', ''):
             possible.append(full_name)
     if len(possible) == 1:
         return possible[0]
@@ -87,8 +106,7 @@ class ObjectConverter(commands.Converter):
     embed_opts = {
         'unit': {
             'footer_fields': ('type',)
-        },
-        'skill': {}
+        }
     }
 
     def __init__(self, object_type: str):
@@ -101,5 +119,5 @@ class ObjectConverter(commands.Converter):
         """Convert an argument."""
         return embed(
             self.object_type, lookup(self.object_type, argument),
-            **type(self).embed_opts[self.object_type]
+            **type(self).embed_opts.get(self.object_type, {})
         )
