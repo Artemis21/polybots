681c02fb4c3e46e29971d935463a230b9af97d16
Artemis21 <hollyshort21@gmail.com>
2020-08-25 00:53:43 +0100

    Search for channels to archive.

--- polytourney20/tools/archiver.py
+++ polytourney20/tools/archiver.py
@@ -7,8 +7,14 @@ import discord
 import jinja2
 
 
+from tools import config, pastebin
+
+
+CONFIG = config.Config()
+
 env = jinja2.Environment(autoescape=jinja2.select_autoescape())
 TEMPLATE = env.from_string('''
+{{ title }}
 {%- for piece in pieces -%}
     {%- if piece.type == 'text' -%}
         <p>{{ piece.value }}</p>
@@ -50,4 +56,19 @@ async def archive_channel(
             for attachment in message.attachments:
                 url = await imgur_upload(attachment.url)
                 pieces.append({'type': 'image', 'value': url})
-    return TEMPLATE.render(pieces=pieces)
+    title = channel.name.replace('-', ' ').title()
+    return pastebin.upload(TEMPLATE.render(pieces=pieces, title=title))
+
+
+async def archive_channels(search: str):
+    """Archive a set of channels by search."""
+    lines = []
+    for channel in CONFIG.guild.text_channels:
+        if search in channel.name:
+            member = discord.utils.find(
+                lambda u: u.name.lower() in channel.name.split('-'),
+                CONFIG.guild.members
+            )
+            link = await archive_channel(channel, member))
+            lines.append(f'{channel.mention}: {link}')
+    return '\n'.join(lines)
