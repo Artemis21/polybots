cb63df3b0a25a03b4f5fc2e114e17ed079474907
Artemis21 <hollyshort21@gmail.com>
2020-12-21 15:24:32 +0000

    Add utilities to view rules.

---
+++ giant-utils/cogs/rules.py
@@ -0,0 +1,117 @@
+"""Commands to view and search for rules."""
+import typing
+
+import discord
+from discord.ext import commands
+
+from tools import menus, rules
+
+
+class Rules(commands.Cog):
+    """Commands to view and search for rules."""
+
+    def __init__(self, bot: commands.Bot):
+        """Store a reference to the bot."""
+        self.bot = bot
+
+    @commands.command(brief='Get a rule by number.')
+    async def rule(self, ctx: commands.Context, number: str):
+        """Get a rule by its number.
+
+        Example: `{{pre}}rule 5.2`
+        """
+        if rules.rule_exists(number):
+            await self.display_rule(number, channel=ctx, user=ctx.author)
+        else:
+            await ctx.send(f'Could not find rule "{number}".')
+
+    @commands.command(brief='See all rules.')
+    async def rules(self,
+            ctx: commands.Context, *, category: typing.Optional[str]):
+        """Get a list of all the rule categories, or the rules in a category.
+
+        Examples:
+        `{{pre}}rules`
+        `{{pre}}rules 5`
+        `{{pre}}rules submitting results`
+        """
+        if not category:
+            await self.full_rule_list(channel=ctx, user=ctx.author)
+        else:
+            if (category_id := rules.get_category(category)):
+                await self.category_index(
+                    category_id, channel=ctx, user=ctx.author
+                )
+            else:
+                await ctx.send(f'Could not find rule category "{category}".')
+
+    async def full_rule_list(self, **menu_options: dict[str, typing.Any]):
+        """Send a list of all rules."""
+        options = rules.get_all_categories_index()
+        await menus.Menu(
+            client=self.bot,
+            title='Giants League Rules',
+            options=options,
+            callback=self.on_category_select,
+            show_emojis=False,
+            **menu_options
+        ).send()
+
+    async def on_category_select(self, event: menus.MenuEvent):
+        """Display a category selected by menu."""
+        event.menu.stop_listening()
+        await self.category_index(
+            event.option, message=event.menu.message, user=event.user
+        )
+
+    async def category_index(self,
+            category_id: str, **menu_options: dict[str, typing.Any]):
+        """Display a category selected by command or menu."""
+        title = rules.get_all_categories_index()[category_id]
+        options = rules.get_category_index(category_id)
+        options['back'] = '◀️ Back'
+        emojis = list(menus.NUMBER_EMOJIS)
+        emojis[len(options) - 1] = '◀️'
+        await menus.Menu(
+            client=self.bot,
+            title=title,
+            options=options,
+            callback=self.on_rule_select,
+            show_emojis=False,
+            emojis=emojis,
+            **menu_options
+        ).send()
+
+    async def on_rule_select(self, event: menus.MenuEvent):
+        """Display a selected rule."""
+        if event.option == 'back':
+            await self.full_rule_list(
+                message=event.menu.message, user=event.user
+            )
+            return
+        event.menu.stop_listening()
+        await self.display_rule(
+            event.option, message=event.menu.message, user=event.user
+        )
+
+    async def display_rule(self,
+            rule_id: str, **menu_options: dict[str, typing.Any]):
+        """Display a rule selected by command or menu."""
+        options = {rule_id.split('.')[0]: rules.get_rule(rule_id)}
+        await menus.Menu(
+            client=self.bot,
+            title=rule_id,
+            options=options,
+            callback=self.on_rule_back,
+            show_emojis=False,
+            emojis=['◀️'],
+            **menu_options
+        ).send()
+
+
+    async def on_rule_back(self, event: menus.MenuEvent):
+        """Go back to a category index."""
+        event.menu.stop_listening()
+        await self.category_index(
+            event.option, message=event.menu.message, user=event.user
+        )
---
+++ giant-utils/tools/menus.py
@@ -0,0 +1,94 @@
+"""A Discord.py utility for reaction-based menus.
+
+This is not suitable for permanent menus (eg. reaction roles) because it
+only works while the original message is in the menu cache.
+"""
+from collections import namedtuple
+import typing
+
+import discord
+
+
+MenuEvent = namedtuple('MenuEvent', ['user', 'option', 'menu'])
+NUMBER_EMOJIS = [
+    *(f'{n}\N{COMBINING ENCLOSING KEYCAP}' for n in range(1, 10)), '🔟'
+]
+
+
+class Menu():
+    """A reaction-based menu."""
+
+    def __init__(self,
+            client: discord.Client, title: str,
+            callback: typing.Coroutine,
+            options: dict[typing.Any, str],
+            channel: typing.Optional[discord.abc.Messageable] = None,
+            message: typing.Optional[discord.Message] = None,
+            show_emojis: bool = True,
+            user: typing.Optional[discord.User] = None,
+            emojis: typing.Optional[list[str]] = None):
+        """Create a new menu.
+
+        If `message` is provided, the menu will re-use that message by editing
+        it. Otherwise, it will send a new message to `channel`.
+
+        The values of `options` will be displayed to the user, the keys will
+        be used in the callback.
+
+        If `user` is provided, only reactions from that user will be accepted.
+
+        `show_emojis` determines whether emojis will be shown alongside each
+        option, or just added as reactions. `emojis` specifies the emojis to
+        use (it should be at least as long as `options`). It defaults to the
+        emojis 1️⃣ to 🔟.
+        """
+        client.add_listener(self.on_reaction_add)
+        self.callback = callback
+        self.client = client
+        self.channel = channel
+        self.message = message
+        self.user = user
+        self.option_ids = list(options)
+        self.emojis = (emojis or NUMBER_EMOJIS)[:len(options)]
+        self.embed = discord.Embed(title=title, description='\n'.join(
+            '{emoji} {description}'.format(
+                emoji=(f'{emoji} ' if show_emojis else ''),
+                description=description
+            ) for emoji, description in zip(self.emojis, options.values())
+        ))
+
+    async def send(self):
+        """Send the menu so that it can be used."""
+        if self.message:
+            await self.message.edit(embed=self.embed)
+            self.message = await self.message.channel.fetch_message(
+                self.message.id
+            )
+            for reaction in self.message.reactions:
+                if reaction.emoji not in self.emojis:
+                    await reaction.clear()
+                else:
+                    async for user in reaction.users():
+                        if self.user == user or not self.user:
+                            await reaction.remove(user)
+        else:
+            self.message = await self.channel.send(embed=self.embed)
+        for emoji in self.emojis:
+            await self.message.add_reaction(emoji)
+
+    async def on_reaction_add(self,
+            reaction: discord.Reaction, user: discord.User):
+        """Process a reaction being added."""
+        if self.user and user != self.user:
+            return
+        if reaction.message != self.message:
+            return
+        if reaction.emoji not in self.emojis:
+            return
+        index = self.emojis.index(reaction.emoji)
+        option = self.option_ids[index]
+        await self.callback(MenuEvent(user, option, self))
+
+    def stop_listening(self):
+        """Stop listening for the menu being used."""
+        self.client.remove_listener(self.on_reaction_add)
