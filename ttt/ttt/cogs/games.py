daa3adfc09f45e1fd6bd49f6fec64d842c108ab9
Artemis21 <artemisdev21@gmail.com>
2021-04-04 00:35:34 +0100

    [ttt] Initial commit.

---
+++ ttt/ttt/cogs/games.py
@@ -0,0 +1,168 @@
+"""Commands for viewing and managing games."""
+from datetime import datetime
+
+import discord
+from discord.ext import commands, tasks
+
+from ..main import checks, config, many, matchmaking
+from ..models import Game, GameType, Player
+
+
+Ctx = commands.Context
+
+
+def yes_or_no(value: bool) -> str:
+    """Convert a boolean value to a human-readable string."""
+    return 'Yes' if value else 'No'
+
+
+class Games(commands.Cog):
+    """Commands for viewing and managing games."""
+
+    def __init__(self, bot: commands.Bot):
+        """Store a reference to the bot."""
+        self.bot = bot
+        self.ready = False
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """Start the recheck loop."""
+        if not self.ready:
+            self.recheck_games.start()
+            self.ready = True
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        """Check for an ELO bot command being run."""
+        if not message.content.startswith('$'):
+            return
+        command, *args = message.content[1:].split()
+        if command not in ('win', 'rename'):
+            return
+        try:
+            game_id = int(args[0])
+        except (IndexError, ValueError):
+            return
+        game = Game.get_or_none(Game.elo_bot_id == game_id)
+        if not game:
+            return
+        await game.reload_from_elo_api()
+        game.recheck_at = datetime.now() + config.ELO_RECHECK_TIME
+        game.save()
+
+    @tasks.loop(seconds=config.ELO_RECHECK_FREQUENCY.total_seconds())
+    async def recheck_games(self):
+        """Recheck all games with pending rechecks."""
+        await Game.recheck_games()
+
+    @commands.command(brief='View a game.', aliases=['g'])
+    async def game(self, ctx: Ctx, game: Game):
+        """View a game by it's ID.
+
+        Example: `{{pre}}game 34`
+        """
+        await ctx.send(embed=game.embed())
+
+    @commands.command(brief='Report your loss.', aliases=['resign', 'l'])
+    @checks.registered()
+    async def loss(self, ctx: Ctx, game: Game):
+        """Resign from a game your are in.
+
+        YOU MUST HAVE ACTUALLY LOST OR RESIGNED.
+        You do not have to do this if the game has already been marked as won.
+
+        Example: `{{pre}}resign 82419`
+        """
+        member = game.get_member(ctx.ttt_player)
+        if member:
+            member.lost = True
+            member.ended_at = datetime.now()
+            member.save()
+            await ctx.send(
+                f'Marked you as having lost game {game.elo_bot_id}.'
+            )
+        else:
+            await ctx.send(f'You are not in game {game.elo_bot_id}.')
+
+    @commands.command(
+        brief='View a game type.', name='game-type', aliases=['gt', 'type']
+    )
+    async def game_type(self, ctx: Ctx, game_type_id: int):
+        """View a game type by ID.
+
+        Example: `{{pre}}game-type 5`
+        """
+        if game_type := GameType.from_id(game_type_id):
+            await ctx.send(embed=discord.Embed(
+                title=f'Game type {game_type_id}',
+                colour=config.COL_ACCENT,
+                description=str(game_type)
+            ))
+        else:
+            await ctx.send(f'No game type found by ID {game_type_id}.')
+
+    @commands.command(
+        brief='Create a game.', name='open-game', aliases=['open', 'o']
+    )
+    @checks.manager()
+    async def open_game(self, ctx: Ctx, *users: commands.Greedy[Player]):
+        """Create a game.
+
+        Example:
+        `{{pre}}create 3 @Artemis @Ramana @Thorin @Legorooj`
+        """
+        if len(users) != 4:
+            await ctx.send('Game must have four players.')
+            return
+        with ctx.typing():
+            game = await Game.create_game(players=users, guild=ctx.guild)
+        await ctx.send(f'Created game (`{game.elo_bot_id}`).')
+
+    @commands.command(
+        brief='Check player compatibility.',
+        name='check-players', aliases=['check', 'c']
+    )
+    @checks.manager()
+    async def check(self, ctx: Ctx, *users: commands.Greedy[Player]):
+        """Check if some players can play together.
+
+        Example:
+        `{{pre}}check @Artemis @Ramana @Thorin @Legorooj`
+        """
+        compatibility = matchmaking.player_compatibility(users)
+        await ctx.send(embed=discord.Embed(
+            title='Player Compatibility Check',
+            colour=config.COL_ACCENT,
+            description=(
+                f'**Games shared:** {compatibility.shared_games}\n'
+                '**Can play new positions?** '
+                f'{yes_or_no(compatibility.new_positions)}\n'
+                '**Can play almost-new positions?** '
+                f'{yes_or_no(compatibility.almost_new_positions)}\n'
+                '**Any from same team?** '
+                f'{yes_or_no(compatibility.common_teams)}'
+            )
+        ))
+
+    @commands.command(
+        brief='Open multiple games.', name='mass-open', aliases=['mo']
+    )
+    @checks.manager()
+    async def mass_open(self, ctx: Ctx, *, users: many.Many[Player]):
+        """Create many games for one round.
+
+        `users` must be a multiple of four users.
+
+        Example:
+        `{{pre}}create 5 @a @b @c @d @1 @2 @3 @4 @i @ii @iii @iv`
+        """
+        print(users)
+        groups = [users[start:start + 4] for start in range(0, len(users), 4)]
+        print(groups)
+        if len(groups[-1]) != 4:
+            await ctx.send('You must specify a multiple of four users.')
+            return
+        with ctx.typing():
+            for group in groups:
+                await Game.create_game(players=group, guild=ctx.guild)
+        await ctx.send(f'Created {len(groups)} games.')
---
+++ ttt/ttt/cogs/players.py
@@ -0,0 +1,170 @@
+"""Commands relating to players and their profiles."""
+from ttt.main.elo_api import EloApiError
+from typing import Optional
+
+from discord.ext import commands
+
+from ..main import checks, matchmaking
+from ..main.paginator import CodeBlockPaginator, EmbedDescriptionPaginator
+from ..models import League, Player, TribeList
+
+
+Ctx = commands.Context
+
+
+def optional_player_arg(ctx: Ctx, arg: Optional[Player]) -> Player:
+    """Return the argument, defaulting to the author."""
+    if player := arg or ctx.ttt_player:
+        return player
+    raise commands.CommandError(
+        f'You not registered (`{ctx.prefix}register`).'
+    )
+
+
+class Players(commands.Cog):
+    """Commands relating to players and their profiles."""
+
+    def __init__(self, bot: commands.Bot):
+        """Store a reference to the bot."""
+        self.bot = bot
+
+    @commands.command(brief='View a player.', aliases=['profile', 'p'])
+    @checks.optional_registered()
+    async def player(self, ctx: Ctx, *, player: Player = None):
+        """View a player's profile (defaults to your own).
+
+        Examples:
+        `{{pre}}profile @Artemis`
+        `{{pre}}p`
+        """
+        player = optional_player_arg(ctx, player)
+        await ctx.send(embed=player.embed())
+
+    @commands.command(brief='Register yourself.', aliases=['r'])
+    async def register(self, ctx: Ctx):
+        """Sign up for the tournament.
+
+        Example: `{{pre}}register`
+        """
+        player = Player.get_or_none(Player.discord_id == ctx.author.id)
+        if player:
+            await ctx.send('You are already registered.')
+        else:
+            player = Player.create(discord_id=ctx.author.id)
+            player.reload_from_discord_member(ctx.author)
+            try:
+                await player.reload_elo_data()
+            except EloApiError:
+                await ctx.send(
+                    'You are not registered with the ELO bot. Do '
+                    '`$setname <mobile name>` to register, then '
+                    f'`{ctx.prefix}update-profile` to update your profile.'
+                )
+            await ctx.send('Registered you!', embed=player.embed())
+
+    @commands.command(brief='View the leaderboard.', aliases=['lb'])
+    async def leaderboard(self, ctx: Ctx):
+        """View the player leaderboard.
+
+        Example: `{{pre}}lb`
+        """
+        lines = []
+        for n, player in enumerate(Player.leaderboard()):
+            lines.append(
+                f'`{n + 1:>2} {player.wins:>4} {player.complete:>8} '
+                f'{player.total - player.complete:>7}` '
+                f'{player.display_name}'
+            )
+        await EmbedDescriptionPaginator(
+            ctx, title='Leaderboard', lines=lines,
+            header='**` # Wins Complete Ongoing` Player**\n'
+        ).setup()
+
+    @commands.command(
+        brief='Add to your tribes.', name='add-tribes',
+        aliases=['add-tribe', 'at']
+    )
+    @checks.registered()
+    async def add_tribes(self, ctx: commands.Context, *, tribes: TribeList):
+        """Add to the list of tribes you own.
+
+        Examples:
+        `{{pre}}add-tribes elyrion yad ven l k p`
+        `{{pre}}at all`
+        """
+        ctx.ttt_player.tribes += tribes
+        ctx.ttt_player.save()
+        await ctx.send('Added to your tribes :thumbsup:')
+
+    @commands.command(
+        brief='Remove from your tribes.', name='remove-tribes',
+        aliases=['remove-tribe', 'rt']
+    )
+    @checks.registered()
+    async def remove_tribes(self, ctx: Ctx, *, tribes: TribeList):
+        """Remove tribes from the list of tribes you own.
+
+        Example: `{{pre}}rt lux pol`
+        """
+        ctx.ttt_player.tribes -= tribes
+        ctx.ttt_player.save()
+        await ctx.send('Removed from your tribes :thumbsup:')
+
+    @commands.command(
+        brief='Get your ELO bot profile.', name='reload-profile',
+        aliases=['rp']
+    )
+    @checks.optional_registered()
+    async def reload_profile(self, ctx: Ctx, *, player: Player = None):
+        """Reload your profile from ELO bot.
+
+        This is useful if you have updated your in game name or timezone.
+        You can also use this for someone else.
+
+        Examples:
+        `{{pre}}rp`
+        `{{pre}}rp @Artemis`
+        """
+        player = optional_player_arg(ctx, player)
+        await player.reload_elo_data()
+        await ctx.send('Reloaded!')
+
+    @commands.command(brief='List players waiting on a level.', aliases=['w'])
+    @checks.manager()
+    async def waiting(self, ctx: Ctx, level: int):
+        """List players waiting for games on a level.
+
+        `level` should be 0, 1, 2 or 3.
+
+        Example: `{{pre}}waiting 2`
+        """
+        if level < 0 or level > 3:
+            await ctx.send('Can only check for level 0, 1, 2 or 3.')
+            return
+        players = matchmaking.players_waiting_on_level(level)
+        lines = []
+        for player in players:
+            if not player.league:
+                team = 'NO TEAM'
+            elif player.league == League.NOVA:
+                team = 'NOVA'
+            else:
+                league = 'JR' if player.league == League.JUNIOR else 'PRO'
+                team = str(player.team)[:4].upper() + league
+            game_summary = (
+                f'{player.total} games ({player.wins} W / {player.losses} '
+                f'L / {player.in_progress} IP)'
+            )
+            games = []
+            for game_id, position in zip(
+                    player.game_ids, player.game_positions):
+                games.append(f'{game_id} - {position}')
+            games = '; '.join(games)
+            lines.append(
+                f'* {player.display_name} ({team}): {game_summary}\n'
+                f'> {games} \n'
+            )
+        await CodeBlockPaginator(
+            ctx, title='', header=f'Players waiting on level {level}',
+            lines=lines
+        ).setup()
