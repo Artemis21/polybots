ca761cae6a99803e57067be723b85a9dfb18da20
Artemis21 <artemisdev21@gmail.com>
2021-05-09 11:39:11 +0100

    [ttt] Add command to view game timeouts.

--- ttt/ttt/main/timeouts.py
+++ ttt/ttt/main/timeouts.py
@@ -1,7 +1,10 @@
 """Tools for managing timeouts."""
+import peewee
+
+import discord
 from discord.ext import commands
 
-from ..models import Game, Player, Timeout
+from ..models import Game, GamePlayer, Player, Timeout
 
 
 def report_timeout(
@@ -23,3 +26,45 @@ def report_timeout(
         reported_by=ctx.ttt_player
     )
     return f'Reported timeout ({game.mention_all()}).'
+
+
+def get_timeouts(game: Game) -> discord.Embed:
+    """Get a list of timeouts for a game as an embed."""
+    Reporter = Player.alias('reporter')
+    return GamePlayer.select().where(GamePlayer.game == game).join(
+        Timeout,
+        peewee.JOIN.LEFT_OUTER,
+        on=Timeout.game_player == GamePlayer.id
+    ).join(
+        Reporter,
+        peewee.JOIN.LEFT_OUTER,
+        on=Timeout.reported_by == Reporter.discord_id
+    ).join(
+        Player,
+        on=GamePlayer.player == Player.discord_id
+    ).group_by(GamePlayer.id)
+
+
+def display_timeouts(game: Game) -> discord.Embed:
+    """Display a list of timeouts-per-player."""
+    players = get_timeouts(game)
+    embed = discord.Embed(title=f'Timeouts for {game.game_name}').set_footer(
+        text=f'ID: {game.elo_bot_id}'
+    )
+    for player in players:
+        lines = []
+        for timeout in player.timeouts:
+            emoji = 'red_circle' if timeout.is_timeout else 'yellow_circle'
+            line = f':{emoji}: <{timeout.screenshot_url}>'
+            if timeout.reported_by:
+                line += f' by **{timeout.reported_by.display_name}**'
+            # Remove milliseconds.
+            time = str(timeout.reported_at).split('.')[0]
+            line += f' at **{time}**'
+            lines.append(line)
+        embed.add_field(
+            name=player.player.display_name,
+            value='\n'.join(lines) or '*No timeouts yet!*',
+            inline=False
+        )
+    return embed
--- ttt/ttt/models/timeouts.py
+++ ttt/ttt/models/timeouts.py
@@ -11,10 +11,12 @@ from .players import Player
 class Timeout(BaseModel):
     """Model to record yellow/red timeouts of players."""
 
-    game_player = peewee.ForeignKeyField(GamePlayer)
+    game_player = peewee.ForeignKeyField(
+        GamePlayer, backref='timeouts', on_delete='CASCADE'
+    )
     screenshot_url = peewee.CharField(max_length=2047)
     is_timeout = peewee.BooleanField()    # True=red, False=yellow
-    reported_by = peewee.ForeignKeyField(Player)
+    reported_by = peewee.ForeignKeyField(Player, on_delete='SET NULL')
     reported_at = peewee.DateTimeField(default=datetime.now)
 
 
