5b9ae64f91567e219e2a292f16486550ff041a23
Artemis21 <artemisdev21@gmail.com>
2021-05-09 11:56:39 +0100

    [ttt] Add command to delete timeouts.

--- ttt/ttt/models/timeouts.py
+++ ttt/ttt/models/timeouts.py
@@ -1,5 +1,9 @@
 """Model to record yellow/red timeouts of players."""
+from __future__ import annotations
+
 from datetime import datetime
+
+from discord.ext import commands
 
 import peewee
 
@@ -18,6 +22,30 @@ class Timeout(BaseModel):
     is_timeout = peewee.BooleanField()    # True=red, False=yellow
     reported_by = peewee.ForeignKeyField(Player, on_delete='SET NULL')
     reported_at = peewee.DateTimeField(default=datetime.now)
+
+    @classmethod
+    async def convert(
+            cls, ctx: commands.Context, raw_argument: str) -> Timeout:
+        """Convert a Discord.py argument to a game."""
+        try:
+            timeout_id = int(raw_argument)
+        except ValueError:
+            raise commands.BadArgument(f'Invalid timeout ID `{raw_argument}`.')
+        timeout = Timeout.get_or_none(Timeout.id == timeout_id)
+        if not timeout:
+            raise commands.BadArgument(f'Timeout ID `{timeout_id}` not found.')
+        return timeout
+
+    @property
+    def summary(self) -> str:
+        """Get a line to summarise the timeout."""
+        emoji = 'red_circle' if self.is_timeout else 'yellow_circle'
+        line = f':{emoji}: <{self.screenshot_url}>'
+        if self.reported_by:
+            line += f' by **{self.reported_by.display_name}**'
+        # Remove milliseconds.
+        time = str(self.reported_at).split('.')[0]
+        return line + f' at **{time}** (*ID: {self.id}*)'
 
 
 db.create_tables([Timeout])
