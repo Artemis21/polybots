fac18d6f782986774dfacdf3165eb2ba72c5276b
Artemis21 <artemisdev21@gmail.com>
2021-07-02 20:07:04 +0100

    [diplo] Ping waitlist role on game create.

--- diplo-bot/main/config.py
+++ diplo-bot/main/config.py
@@ -12,5 +12,6 @@ PREFIX = _data['prefix']
 TOKEN = _data['token']
 
 OBSERVER_ROLE_ID = _data['observer_role']
+WAITLIST_ROLE_ID = _data['waitlist_role']
 ADMIN_ROLE_IDS = _data.get('admin_roles', [])
 TRIBE_EMOJI_IDS = _data.get('tribe_emojis', {})
